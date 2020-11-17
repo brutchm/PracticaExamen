c16c12356dfbf6978973a4c81e2695df2776dc7d
Brandon Rutch Murillo <trickered@Brandons-MacBook-Pro.local>
2020-11-17 17:58:58 -0600

    se sube la practica

---
+++ src/main/java/com/cenfotec/crud/service/AntologyService.java
@@ -0,0 +1,18 @@
+package com.cenfotec.crud.service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import com.cenfotec.crud.domain.Antology;
+
+public interface AntologyService {
+
+	public void save(Antology antology);
+	public Optional<Antology> get(Long id);
+	public List<Antology> getAll();
+	public List<Antology> find(String name);
+	public List<Antology> findAuthor(String author);
+	public List<Antology> findRelease(String release);
+	
+}
